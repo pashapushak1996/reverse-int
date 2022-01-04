258d72948285ff312206c47534e181b96a2d0352
pashapushak1996 <pashapushak1996@gmail.com>
2022-01-04 21:41:03 +0200

    init: reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (int) {
+    const stringOfNumbers = [...int.toString()].reverse();
+
+    let reversedNumbers = null;
+
+    if (stringOfNumbers.includes('-')) {
+
+        reversedNumbers = stringOfNumbers.splice(0, stringOfNumbers.length - 1).join('');
+
+        return Number(reversedNumbers);
+    }
+
+    return Number(stringOfNumbers.join(''));
 }
